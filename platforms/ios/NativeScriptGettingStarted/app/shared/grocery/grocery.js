d2c0545ab991790c2beded490bfc01ce8228ab99
Temi Lajumoke <temi@temilajumoke.com>
2017-05-20 14:26:36 +0100

    add platforms

---
+++ platforms/ios/NativeScriptGettingStarted/app/shared/grocery/grocery.js
@@ -0,0 +1,1 @@
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiZ3JvY2VyeS5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbImdyb2NlcnkudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsInNvdXJjZXNDb250ZW50IjpbIiJdfQ==
