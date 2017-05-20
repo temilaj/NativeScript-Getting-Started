f09163d5412404a5a2f722519c43efedc5c5c663
Temi Lajumoke <temi@temilajumoke.com>
2017-05-20 14:54:15 +0100

    implement user service

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,14 +1,48 @@
 import { Component } from "@angular/core";
 import { User } from "./shared/user/user";
+
+import { UserService } from "./shared/user/user.service";
+
 @Component({
   selector: "my-app",
-  templateUrl: "app.component.html"
+  providers: [UserService],
+  templateUrl: "pages/login/login.html",
+  styleUrls: ["pages/login/login-common.css", "pages/login/login.css"],
 })
 export class AppComponent {
   user: User;
   isLoggingIn = true;
 
-  constructor() {
+  constructor(private router: UserService) {
     this.user = new User();
+  }
+  submit() {
+    // alert(`You’re using: ${this.user.email}  and password ${this.user.password}`);
+    if (this.isLoggingIn) {
+      this.login();
+    } else {
+      this.signUp();
+    }
+  }
+  toggleDisplay() {
+    this.isLoggingIn = !this.isLoggingIn;
+  }
+
+  login() {
+    this.userService.login(this.user)
+      .subscribe(
+        () => this.router.navigate(["/list"]),
+        (error) => alert("Unfortunately we could not find your account.")
+      );
+  }
+  signUp() {
+  this.userService.register(this.user)
+    .subscribe(
+      () => {
+        alert("Your account was successfully created.");
+        this.toggleDisplay();
+      },
+      () => alert("Unfortunately we were unable to create your account.")
+    );
   }
 }
--- platforms/ios/NativeScriptGettingStarted/app/app.component.ts
+++ platforms/ios/NativeScriptGettingStarted/app/app.component.ts
@@ -1,8 +1,13 @@
 import { Component } from "@angular/core";
 import { User } from "./shared/user/user";
+
+import { UserService } from "./shared/user/user.service";
+
 @Component({
   selector: "my-app",
-  templateUrl: "app.component.html"
+  providers: [UserService],
+  templateUrl: "pages/login/login.html",
+  styleUrls: ["pages/login/login-common.css", "pages/login/login.css"],
 })
 export class AppComponent {
   user: User;
