66c681c35fe129f3d5cac3340ebdd4a068df6750
Temi Lajumoke <temi@temilajumoke.com>
2017-05-27 21:42:59 +0100

    add native colors

--- platforms/ios/NativeScriptGettingStarted/app/pages/login/login.component.ts
+++ platforms/ios/NativeScriptGettingStarted/app/pages/login/login.component.ts
@@ -8,6 +8,9 @@ import { View } from "ui/core/view";
 import { User } from "../../shared/user/user";
 import { UserService } from "../../shared/user/user.service";
 
+import { setHintColor } from "../../utils/hint-util";
+import { TextField } from "ui/text-field";
+
 @Component({
   selector: "my-app",
   providers: [UserService],
@@ -18,12 +21,28 @@ export class LoginComponent implements OnInit {
   user: User;
   isLoggingIn = true;
   @ViewChild("container") container: ElementRef;
+  @ViewChild("email") email: ElementRef;
+  @ViewChild("password") password: ElementRef;
+
 
   constructor(private router: Router, private userService: UserService, private page: Page) {
     this.user = new User();
     this.user.email = "user@nativescript.org";
     this.user.password = "password";
   }
+  setTextFieldColors() {
+    let emailTextField = <TextField>this.email.nativeElement;
+    let passwordTextField = <TextField>this.password.nativeElement;
+  
+    let mainTextColor = new Color(this.isLoggingIn ? "black" : "#C4AFB4");
+    emailTextField.color = mainTextColor;
+    passwordTextField.color = mainTextColor;
+  
+    let hintColor = new Color(this.isLoggingIn ? "#ACA6A7" : "#C4AFB4");
+    setHintColor({ view: emailTextField, color: hintColor });
+    setHintColor({ view: passwordTextField, color: hintColor });
+  }
+  
   submit() {
     // alert(`You’re using: ${this.user.email}  and password ${this.user.password}`);
     if(typeof(this.user.email) == "undefined" || typeof(this.user.password) == "undefined" ||this.user.email.length < 3 || this.user.password.length < 3)
@@ -45,6 +64,7 @@ export class LoginComponent implements OnInit {
   toggleDisplay() {
     this.isLoggingIn = !this.isLoggingIn;
     let container = <View>this.container.nativeElement;
+    this.setTextFieldColors();
     container.animate({
       backgroundColor: this.isLoggingIn ? new Color("white") : new Color("#e1e9e9"),
       duration: 500
