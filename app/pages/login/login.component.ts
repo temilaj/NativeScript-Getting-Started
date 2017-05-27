8dbd12d28fce5eca7e371e945e9b9fa872dc8168
Temi Lajumoke <temi@temilajumoke.com>
2017-05-27 18:28:34 +0100

    add input field validation

--- app/pages/login/login.component.ts
+++ app/pages/login/login.component.ts
@@ -30,32 +30,49 @@ export class LoginComponent implements OnInit {
       this.signUp();
     }
   }
+
   toggleDisplay() {
     this.isLoggingIn = !this.isLoggingIn;
     let container = <View>this.container.nativeElement;
     container.animate({
-      backgroundColor: this.isLoggingIn ? new Color("white") : new Color("#322122"),
+      backgroundColor: this.isLoggingIn ? new Color("white") : new Color("#e1e9e9"),
       duration: 500
     });
   }
 
   login() {
-    this.userService.login(this.user)
-      .subscribe(
-        () => this.router.navigate(["/list"]),
-        (error) => alert("Unfortunately we could not find your account.")
-      );
+    if(typeof(this.user.email) == "undefined" || typeof(this.user.password) == "undefined" ||this.user.email.length < 3 || this.user.password.length < 3)
+    {
+      alert("Enter a valid username or password");
+    }
+    else 
+    {
+      this.userService.login(this.user)
+        .subscribe(
+          () => this.router.navigate(["/list"]),
+          (error) => alert("Unfortunately we could not find your account.")
+        );
+    }
   }
+
   signUp() {
-  this.userService.register(this.user)
-    .subscribe(
-      () => {
-        alert("Your account was successfully created.");
-        this.toggleDisplay();
-      },
-      () => alert("Unfortunately we were unable to create your account.")
-    );
+    if(typeof(this.user.email) == "undefined" || typeof(this.user.password) == "undefined" ||this.user.email.length < 3 || this.user.password.length < 3)
+    {
+      alert("Enter a valid username or password");
+    }
+    else
+    {
+      this.userService.register(this.user)
+        .subscribe(
+          () => {
+            alert("Your account was successfully created.");
+            this.toggleDisplay();
+          },
+          () => alert("Unfortunately we were unable to create your account.")
+        );
+    }
   }
+
   ngOnInit(): void {
     this.page.actionBarHidden = true;
     this.page.backgroundImage = "res://bg_login";
--- platforms/ios/NativeScriptGettingStarted/app/shared/user/user.service.ts
+++ platforms/ios/NativeScriptGettingStarted/app/shared/user/user.service.ts
@@ -28,26 +28,26 @@ export class UserService {
     .catch(this.handleErrors);
     // .map(res => res.json());
   }
+
   login(user: User) {
-  let headers = new Headers();
-  headers.append("Content-Type", "application/json");
-
-  return this.http.post(
-    Config.apiUrl + "oauth/token",
-    JSON.stringify({
-      username: user.email,
-      password: user.password,
-      grant_type: "password"
-    }),
-    { headers: headers }
-  )
-  .map(response => response.json())
-  .do(data => {
-    Config.token = data.Result.access_token;
-  })
-  .catch(this.handleErrors);
-}
-
+    let headers = new Headers();
+    headers.append("Content-Type", "application/json");
+  
+    return this.http.post(
+      Config.apiUrl + "oauth/token",
+      JSON.stringify({
+        username: user.email,
+        password: user.password,
+        grant_type: "password"
+      }),
+      { headers: headers }
+    )
+    .map(response => response.json())
+    .do(data => {
+      Config.token = data.Result.access_token;
+    })
+    .catch(this.handleErrors);
+  }
 
   handleErrors(error: Response) {
     console.log(JSON.stringify(error.json()));
